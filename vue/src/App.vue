bcd3add1f04192e8dd83a6f3073c3299573089b2
Clare Beany <clarebeany@gmail.com>
2021-12-06 22:03:21 -0500

    background and image for splash page

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,31 +1,44 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link class="router-link" v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link class="router-link" v-bind:to="{ name: 'login'}">Login</router-link>&nbsp;|&nbsp;
-      <router-link class="router-link" v-bind:to="{ name: 'register'}">Register</router-link>&nbsp;|&nbsp;
-      <router-link class="router-link" v-bind:to="{ name: 'contact'}">Contact</router-link>&nbsp;|&nbsp;
-      <router-link class="router-link" v-bind:to="{ name: 'about'}">About</router-link>&nbsp;|&nbsp;
-      <router-link class="router-link" v-bind:to="{ name: 'properties'}">Properties</router-link>|&nbsp;
-      <router-link class="router-link" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>&nbsp;
+      <router-link class="router-link" v-bind:to="{ name: 'home' }"
+        >Home</router-link
+      >&nbsp;|&nbsp;
+      <router-link class="router-link" v-bind:to="{ name: 'properties' }"
+        >Properties</router-link
+      >|&nbsp;
+      <router-link class="router-link" v-bind:to="{ name: 'login' }"
+        >Login</router-link
+      >&nbsp;|&nbsp;
+      <router-link class="router-link" v-bind:to="{ name: 'register' }"
+        >Register</router-link
+      >&nbsp;|&nbsp;
+      <!-- <router-link class="router-link" v-bind:to="{ name: 'contact'}">Contact</router-link>&nbsp;|&nbsp;
+      <router-link class="router-link" v-bind:to="{ name: 'about'}">About</router-link>&nbsp;|&nbsp; -->
+
+      <router-link
+        class="router-link"
+        v-bind:to="{ name: 'logout' }"
+        v-if="$store.state.token != ''"
+        >Logout</router-link
+      >&nbsp;
     </div>
     <router-view />
   </div>
 </template>
 <style>
-
 #nav {
   display: flex;
   justify-content: space-between;
-  background-color:rgba(255, 255, 255, 0.774);
-  margin: 0;
-  padding: 50px;
+  background-color: rgba(255, 255, 255, 0.774);
+  margin: auto;
+  padding: 5% auto 5% auto;
   width: 100%;
 }
 
 .router-link {
   text-decoration: none;
-  color: #101411; 
+  color: #101411;
   margin: auto;
   width: 100%;
 }
@@ -42,8 +55,6 @@
   color: #2c3e50;
   max-width: 1000px;
   margin: 0 auto 0 auto;
- 
+
 }
-
-
 </style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,28 +1,38 @@
 <template>
   <div class="home">
     <body>
-       <h1>Welcome to Elevate Real Estate</h1>
-    <p>Elevate your quality of home with us</p>
-    <div class="apt-pics">
-      <v-img src="../assets/FL.jpg" alt="apartment complex pic" max-height="100" max-width="150"></v-img>
+      <h1>Welcome to Elevate Real Estate</h1>
+      <p>Find your perfect home to elevate your quality of home with us.</p>
+      <div class="apt-pics">
+        <img src="../assets/FL.jpg" alt="apartment complex pic" />
       </div>
     </body>
-   
   </div>
 </template>
 
 <script>
-
 export default {
-  name: "home"
+  name: "home",
 };
 </script>
 <style>
+body {
 
+  color: rgb(237, 218, 214);
+  text-align: center;
+  background-image: linear-gradient(black, grey);
+  background-repeat: no-repeat;
+}
 
-  body{
-    background: black;
-    color: rgb(237, 218, 214);
-    }
-    
+img {
+
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+  border: 1rem thin pink;
+  border-style: solid;
+  border-radius: 5px;
+
+}
 </style>
