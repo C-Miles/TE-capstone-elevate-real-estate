6989bf0b3253a7114c45480cbe41c4181b1ef0c5
Clare Beany <clarebeany@gmail.com>
2021-12-07 17:03:15 -0500

    splash page nav bar & image with properties

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,18 +1,17 @@
 <template>
-  <div id="app">
-    <div id="nav">
+
+  <div class="wrapper" id="app">
+    <section id="top=panel">
+    <nav id="nav">
       <router-link class="router-link" v-bind:to="{ name: 'home' }"
         >Home</router-link
-      >&nbsp;|&nbsp;
-      <router-link class="router-link" v-bind:to="{ name: 'properties' }"
-        >Properties</router-link
-      >|&nbsp;
+      >
       <router-link class="router-link" v-bind:to="{ name: 'login' }"
         >Login</router-link
-      >&nbsp;|&nbsp;
+      >
       <router-link class="router-link" v-bind:to="{ name: 'register' }"
         >Register</router-link
-      >&nbsp;|&nbsp;
+      >
       <!-- <router-link class="router-link" v-bind:to="{ name: 'contact'}">Contact</router-link>&nbsp;|&nbsp;
       <router-link class="router-link" v-bind:to="{ name: 'about'}">About</router-link>&nbsp;|&nbsp; -->
 
@@ -22,34 +21,60 @@
         v-if="$store.state.token != ''"
         >Logout</router-link
       >&nbsp;
-    </div>
+    </nav>
+    </section>
+    <section id="bottom-panel">
+        
+    </section>
     <router-view />
   </div>
 </template>
 <style>
-#nav {
-  display: flex;
-  justify-content: space-between;
-  background-color: rgba(255, 255, 255, 0.774);
-  margin: auto;
-  padding: 5% auto 5% auto;
-  width: 100%;
+
+.wrapper {
+	display: grid;
+	grid-gap: 20px;
+
 }
 
+	#top-panel {
+    display: flex;
+    flex-basis: 35%;
+    
+  }
+
+  #bottom-panel {
+    display: flex;
+    flex-grow: 1;
+    justify-content: center;
+    justify-items: center;
+  }
+  
+
 .router-link {
+  justify-content: space-between;
+  background-color: rgba(255, 255, 255, 0.774);
   text-decoration: none;
-  color: #101411;
-  margin: auto;
-  width: 100%;
+  color:black;
+	display: inline-block; 
+	font-size: 1.1rem;
+	padding: 0.8rem;
+  margin: 20px;
+  height: 1rem;
+  width: 8rem;
+  text-decoration: none;
+  border-radius: 5px; 
+	text-align: center;
+	text-decoration: none;
+	text-transform: uppercase;
 }
 
 .router-link:hover {
-  text-decoration: underline;
-  color: blue;
+  background: rgb(73, 73, 73);
+  color: white
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-
   text-align: center;
   color: #2c3e50;
   max-width: 1000px;
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="home">
-    <body>
+    <body class="gradient">
       <h1>Welcome to Elevate Real Estate</h1>
-      <p>Find your perfect home to elevate your quality of home with us</p>
+      <p class="description">
+        Find your perfect home to elevate your quality of home with us
+      </p>
+
       <div class="apt-pics">
-        <img src="../assets/FL.jpg" alt="apartment complex pic" />
+        <section class="testing">
+          <h2>Find your dream rental</h2>
+          <p>View your possibilities today that fit your desires</p>
+           <router-link
+            id="properties-link"
+            class="router-link"
+            v-bind:to="{ name: 'properties' }"
+            >Properties</router-link
+          >
+        </section>
+        <!-- <img src="../assets/FL.jpg" alt="apartment complex pic" /> -->
       </div>
     </body>
   </div>
@@ -16,18 +29,42 @@ export default {
 };
 </script>
 <style>
-h1{
-  font-family: 'Baloo Bhaijaan 2', cursive,  Avenir, Helvetica, Arial, sans-serif;
+h1 {
+  font-family: "Baloo Bhaijaan 2", Avenir, Helvetica, Arial, sans-serif;
+  font-weight: bold;
+  font-size: 40px;
+  text-shadow: 2px 2px #00d9ff;
 }
-body {
+
+.description {
+  font-family: "Montserrat", sans-serif;
+}
+
+body.gradient {
+  /*
+box-sizing: border-box;
+line-height: 1.4; 
+	margin: 30px 50px;
+  */
 
   color: rgb(237, 218, 214);
   text-align: center;
-  background: black;
+  /* background-image: linear-gradient(black, grey); */
+}
+
+.testing {
+  display: block;
+  background: url(../assets/FL.jpg) right / cover no-repeat;
+  padding: 10rem;
+  width: 100%;
+  position: fixed;
+  top: 25rem;
+  left: 0;
+ /* opacity: 80%; */
 }
 
 img {
-
+  /*
   display: block;
   margin-left: auto;
   margin-right: auto;
@@ -35,6 +72,6 @@ img {
   border: 1rem thin pink;
   border-style: solid;
   border-radius: 5px;
-
+*/
 }
 </style>
