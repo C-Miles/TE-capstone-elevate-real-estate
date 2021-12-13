a0d1f39f450c4f6693f85823e0cbc948357321c1
Jillian Augustat <jillian.augustat@gmail.com>
2021-12-13 14:53:13 -0500

    Adding Maps

--- vue/src/main.js
+++ vue/src/main.js
@@ -4,7 +4,7 @@ import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
 import * as VueGoogleMaps from "vue2-google-maps"
-
+import Geocoder from "@pderas/vue2-geocoder";
 
 Vue.config.productionTip = false
 
@@ -12,11 +12,17 @@ axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: "AIzaSyBfDsfLr_iDcwa-v8Fsr8LCP7R0_05HhdU",
+    key: "AIzaSyASVTWiCp6rjLMiqh6az1jpvtW3iYGMPT8",
     libraries: "places"
   }
-})
+}),
 
+Vue.use(Geocoder, {
+  defaultCountryCode: null, // e.g. 'CA'
+  defaultLanguage:    null, // e.g. 'en'
+  defaultMode:        'address', // or 'lat-lng'
+  googleMapsApiKey: "AIzaSyASVTWiCp6rjLMiqh6az1jpvtW3iYGMPT8"
+})
 
 new Vue({
   router,
