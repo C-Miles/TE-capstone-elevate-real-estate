cab0be16e3848a86c393592f0bb4e542fe9bbcd4
Sonam Sherpa <sonam.sherpa34@gmail.com>
2021-12-14 09:05:06 -0500

    testing

--- java/src/test/java/com/techelevator/dao/DAOApplicationTest.java
+++ java/src/test/java/com/techelevator/dao/DAOApplicationTest.java
@@ -1,6 +1,8 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.Application;
 import org.junit.Before;
+import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class DAOApplicationTest extends DAOIntegrationTest{
@@ -8,13 +10,31 @@ public class DAOApplicationTest extends DAOIntegrationTest{
         private JdbcApplicationDAO jdbcApplicationDAO;
         private JdbcTemplate jdbcTemplate;
 
-        @Before
+    @Before
     public void setUp() {
 
         jdbcTemplate = new JdbcTemplate(getDataSource());
         jdbcApplicationDAO = new JdbcApplicationDAO(getDataSource());
     }
+    @Test
+    public void get_application() {
+        
+    }
 
+    private Application insertApplication(long rentAppId, String firstName, String lastName, String emailAddress, String phoneNumber) {
+     Application application = new Application();
+     application.setRentAppId(rentAppId);
+     application.setFirstName(firstName);
+     application.setLastName(lastName);
+     application.setEmailAddress(emailAddress);
+     application.setPhoneNumber(phoneNumber);
+
+     String sql = "INSERT INTO application(rent_app_id, first_name, last_name, email_address, phone_number) VALUES (DEFAULT, 'sonam', 'miles', 'johndoe@aol.com', 1234567890) RETURNING rent_app_id";
+    rentAppId = jdbcTemplate.queryForObject(sql, Long.class, firstName, lastName, emailAddress, phoneNumber);
+    application.setRentAppId(rentAppId);
+    return application;
+
+    }
 
 
 
